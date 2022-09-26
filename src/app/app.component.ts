21ff8b3f32cf14425775462a13c3a10400815a68
etcllr <76406627+etcllr@users.noreply.github.com>
2022-09-26 16:00:11 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { OnInit, Component, ElementRef, ViewChild } from '@angular/core';
 import {
   CdkDragDrop,
   CdkDragEnter,
